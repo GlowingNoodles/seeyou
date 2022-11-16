2c0f100e810a7483126646ee8612c2dfeafb11d7
fgmtGit <djhwjzfh@163.com>
2022-11-16 16:12:47 +0800

    我的第一个项目，上传到git上面

---
+++ src/test/java/com/seeyou/SeeyouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seeyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeeyouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
